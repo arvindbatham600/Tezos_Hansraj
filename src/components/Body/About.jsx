f8ebc29145df2ede9736c60bbaee4ea10d105921
bhawna <bhawnabc2002@gmail.com>
2022-10-20 23:36:25 +0530

    added content

--- src/components/Body/About.jsx
+++ src/components/Body/About.jsx
@@ -38,8 +38,8 @@ function About() {
               padding: "20px",
             }}
           >
-            Cerritus Coders is a student-run community focusing on open-source,
-            project-based learning. It aims to help folks to get opportunities
+            Tezos_Hansraj is a student-run community focusing on learning more about Web3.
+             It aims to help folks to get opportunities
             by encouraging them to learn, build and share in public.
           </Typography>
           <Typography
@@ -48,13 +48,14 @@ function About() {
               padding: "20px",
             }}
           >
-            Cerritus Coders is a student-run community focusing on open-source,
-            project-based learning. It aims to help folks to get opportunities
-            by encouraging them to learn, build and share in public.
+          We encourage folks to get started with 
+          Blockchain Development and conduct multiple events 
+          around the same. We are a community
+           of more than 100+ members and growing every day.
           </Typography>
         </Box>
         <Box className="image1">
-          <img width={300} src={image1} alt="image " />
+          <img width={500} src={image1} alt="image " />
         </Box>
       </Box>
     </Box>
--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -31,13 +31,8 @@ function Body() {
           }}
           variant="h5"
         >
-          lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Let's Build the community of Blockchain Developers, where we all grow together,
+          encourage each other for development and Unlock their full potentials that makes, one step more towards your goal.
         </Typography>
       </Box>
     </Box>
